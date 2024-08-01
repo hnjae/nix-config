81fe61310f2ffd18f0224e2de72c07556ccc7b1c
KIM Hyunjae <hnjae@outlook.com>
2024-08-01 20:54:16 +0900

    feat(nixos): add kde-pim apps

--- nixos-modules/de-plasma6/packages/kde-apps.nix
+++ nixos-modules/de-plasma6/packages/kde-apps.nix
@@ -1,5 +1,13 @@
 {pkgs, ...}: {
-  programs.partition-manager.enable = true;
+  programs = {
+    partition-manager.enable = true;
+    kde-pim = {
+      enable = true;
+      kmail = true;
+      kontact = true;
+      merkuro = true;
+    };
+  };
 
   environment.defaultPackages = with pkgs; [
     # kde-apps
