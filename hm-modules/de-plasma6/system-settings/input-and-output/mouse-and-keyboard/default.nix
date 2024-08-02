23987432a1dda4b306101f9130237b93a5e0c6eb
KIM Hyunjae <hnjae@outlook.com>
2024-08-02 14:51:15 +0900

    mod(kde): exclude kcminputrc from plasma-manager's resetfiles

--- hm-modules/de-plasma6/system-settings/input-and-output/mouse-and-keyboard/default.nix
+++ hm-modules/de-plasma6/system-settings/input-and-output/mouse-and-keyboard/default.nix
@@ -1,1 +1,6 @@
-{...}: {imports = [./touchpad.nix ./mouse.nix];}
+{...}: {
+  imports = [./touchpad.nix ./mouse.nix];
+
+  # to keep color appearance
+  programs.plasma.resetFilesExclude = ["kcminputrc"];
+}
