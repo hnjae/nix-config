fe4cdc88ecb8488fe0205b66a35adf7caa400589
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-08-02 15:04:11 +0900

    feat(dev): new flatpak program - sequeler

--- hm-modules/generic-home/programs/70-dev/db.nix
+++ hm-modules/generic-home/programs/70-dev/db.nix
@@ -7,11 +7,10 @@
   genericHomeCfg = config.generic-home;
 in {
   config = lib.mkIf genericHomeCfg.installDevPackages {
-    home.packages = with pkgs; [mongosh sqlite];
+    home.packages = with pkgs; [mongosh];
 
     services.flatpak.packages = lib.mkIf genericHomeCfg.isDesktop [
       "io.dbeaver.DBeaverCommunity" # db tool
-      # "com.github.alecaddd.sequeler" # sql client, uses end-of-life dependency
     ];
   };
 }
