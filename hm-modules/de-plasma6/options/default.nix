e20c7cb69519d78d58222bd275f40c11685a0a13
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-08-02 15:31:21 +0900

    refactor(kde): move default-app settings to a separate file

--- hm-modules/de-plasma6/options/default.nix
+++ hm-modules/de-plasma6/options/default.nix
@@ -1,19 +1,6 @@
 {...}: {
   imports = [
+    ./default-app.nix
     ./stateful.nix
   ];
-  default-app = {
-    image = "org.kde.gwenview";
-    fileManager = "org.kde.dolphin";
-    fromApps = [
-      "org.kde.konsole"
-      # "org.kde.dolphin" dolphin 은 portal 안쓰는듯??? <NixOS 24.05>
-    ];
-    mime = {
-      "application/pdf" = "org.kde.okular";
-      "application/epub+zip" = "org.kde.okular";
-      "application/x-mobipocket-ebook" = "org.kde.okular";
-    };
-    archive = "org.kde.ark";
-  };
 }
