dd84116ff5bdd3e8334dffa768c9f4f05b1e6bd4
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-08-07 10:32:13 +0900

    fix(oci-container-auto-update): handle cases where multiple containers use the same image

--- nixos-modules/services/oci-container-auto-update/package/default.nix
+++ nixos-modules/services/oci-container-auto-update/package/default.nix
@@ -9,6 +9,7 @@ NOTE: <NixOS 24.05>
   pkgs,
   name,
   image,
+  containerName,
   targetService,
 }:
 # e.g.) image = "docker.io/lobehub/lobechat:latest"
@@ -18,7 +19,7 @@ pkgs.writeShellApplication {
   runtimeInputs = with pkgs; [
     podman
     jq
-    coreutils
+    # coreutils
     systemd
   ];
 
@@ -36,23 +37,40 @@ pkgs.writeShellApplication {
     fi
 
     prev_img=''$(
-      podman images --format json |
-        jq -r '.[] | select(.Names | index("${image}")) | .Id' |
-        uniq
+      podman ps --format json |
+        jq -r '.[] | select(.Names | index("${containerName}")) | .ImageID'
     )
+
+    if [ -z "''$prev_img" ]; then
+      echo "ERROR: ${containerName} is not running"
+      exit 1
+    fi
+
     new_img=''$(podman pull "${image}" 2>/dev/null)
 
     if [ "''$prev_img" = "''$new_img" ]; then
-      echo "INFO: ${image} is up to date"
+      echo "INFO: ${containerName}'s image is up to date"
       exit 0
     fi
 
-    echo "INFO: detected updates of ${image}"
+    echo "INFO: detected updates of ${containerName}"
 
     echo "INFO: restarting service ${targetService}"
     systemctl restart "${targetService}"
 
-    echo "INFO: removing previous image of ${image} (''${prev_img})"
+    #  다른 컨테이너가 해당 이미지를 사용하는지 확인하고 지우기.
+    other_cons=''$(
+      podman ps --format json |
+        jq -r ".[] | select(.ImageID | index(\"''${prev_img}\")) | .Id"
+    )
+
+    prev_img_c=''$(echo "''$prev_img" | cut -c -12)
+    if [ -n "''$other_cons" ]; then
+      echo "INFO: previous image of ${containerName} (''${prev_img_c}) is being used by other containers"
+      exit 0
+    fi
+
+    echo "INFO: removing previous image of ${containerName} (''${prev_img_c})"
     podman rmi "''$prev_img" 2>/dev/null
   '';
 }
