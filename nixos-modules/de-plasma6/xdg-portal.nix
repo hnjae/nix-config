f9d480640737aa0f22c0a2a4c8a53ea875fa52cd
KIM Hyunjae <hnjae@outlook.com>
2024-08-07 17:40:54 +0900

    fix(plasma6): re-enable xdg-desktop-portal-gtk on kde

--- nixos-modules/de-plasma6/xdg-portal.nix
+++ nixos-modules/de-plasma6/xdg-portal.nix
@@ -13,7 +13,6 @@ in {
   xdg.portal.extraPortals = with pkgs; [
     # libsForQt5.xdg-desktop-portal-kde
 
-    # xdg-desktop-portal-gtk 없으면 gtk 앱에서 antialasing+cursor theme 안됨. <NixOS 23.05>
-    # xdg-desktop-portal-gtk
+    xdg-desktop-portal-gtk # 없으면 gtk 앱에서 antialasing+cursor theme 안됨. <NixOS 23.05>
   ];
 }
