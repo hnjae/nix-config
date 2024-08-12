3acc3aa423ab4e08e69df563b54f746177ba5444
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-08-12 15:43:31 +0900

    feat(lact): add new nixos-modules that enable lact

--- nixos-modules/flake-module.nix
+++ nixos-modules/flake-module.nix
@@ -12,11 +12,15 @@
       nix-gc = import ./services/nix-gc;
       oci-container-auto-update = import ./services/oci-container-auto-update;
 
+      #
+      lact = import ./programs/lact.nix;
+
       default = {
         imports = [
           self.nixosModules.generic-nixos
           self.nixosModules.nix-gc
           self.nixosModules.oci-container-auto-update
+          self.nixosModules.lact
         ];
       };
     };
