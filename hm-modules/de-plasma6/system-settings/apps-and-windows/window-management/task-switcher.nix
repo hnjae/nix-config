49c55338a3aa3f6373d2ad7a897ef05b26635347
KIM Hyunjae <hnjae@outlook.com>
2024-08-13 19:00:23 +0900

    feat(generic-nixos): disable altwin:swap_lalt_lwin

--- hm-modules/de-plasma6/system-settings/apps-and-windows/window-management/task-switcher.nix
+++ hm-modules/de-plasma6/system-settings/apps-and-windows/window-management/task-switcher.nix
@@ -4,5 +4,10 @@
     "Walk Through Windows (Reverse)" = ["Meta+Shift+Tab"];
     "Walk Through Windows of Current Application" = ["Meta+`"];
     "Walk Through Windows of Current Application (Reverse)" = ["Meta+~"];
+
+    "Walk Through Windows Alternative" = ["Alt+Tab"];
+    "Walk Through Windows Alternative (Reverse)" = ["Alt+Shift+Tab"];
+    "Walk Through Windows of Current Application Alternative" = ["Alt+`"];
+    "Walk Through Windows of Current Application Alternative (Reverse)" = ["Alt+~"];
   };
 }
