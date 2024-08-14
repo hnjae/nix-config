1676375a58107429635d84e1a209d95fc1de5a8e
KIM Hyunjae <hnjae@outlook.com>
2024-08-14 17:27:49 +0900

    feat(zsh): overhaul zsh config

--- hm-modules/generic-home/programs/00-shell/zsh/default.nix
+++ hm-modules/generic-home/programs/00-shell/zsh/default.nix
@@ -1,13 +1,22 @@
 {
   config,
   pkgsUnstable,
-  lib,
   ...
 }: let
   # inherit (config.home) shellAliases;
   # inherit (lib.strings) optionalString;
   concat = builtins.concatStringsSep "\n";
 in {
+  imports = [
+    # ./zsh-vi-mode.nix
+    ./profile.nix
+  ];
+
+  home.packages = [
+    # provides various /nix/store/<hash>/share/zsh/site-functions
+    pkgsUnstable.zsh-completions
+  ];
+
   # NOTE: source order:
   # .zshenv, .zprofile, .zlogin(optional?), .zshrc | .zlogout
   # home.sessionVariables 는 .zshenv의 머리에,
@@ -22,44 +31,39 @@ in {
   programs.zsh = {
     enable = true;
 
+    dotDir = ".config/zsh";
+
     # NOTE: envExtra being ignored or override for unknown reason <2023-03-26>
     # .zshenv 말미
-    envExtra = concat [
-      ''
-        ZVM_CURSOR_STYLE_ENABLED=false
-      ''
-    ];
+    # envExtra = "";
 
     # .zprofle
-    profileExtra = lib.mkOrder 1 ''
-      [ -n "$__PROFILE_SOURCED" ] && return
-      __PROFILE_SOURCED=1
-    '';
+    # profileExtra = "";
 
     # .zshrc 중간 (after zplugin, history)
     initExtra = concat [
       ''
-        # plugins
-        # zsh-vi-mode uses C-r
-        # . "${pkgsUnstable.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
 
         # history 에서 일치하는 명령 줄 배경색으로 표기
         . "${pkgsUnstable.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
         # alias 에 있는 커맨드와 동일하면 표기
         . "${pkgsUnstable.zsh-you-should-use}/share/zsh/plugins/you-should-use/you-should-use.plugin.zsh"
+
+        # syntax highlighting
         . "${pkgsUnstable.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh"
+
         . "${pkgsUnstable.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh"
+
+        # run `zhooks` to display functions and array <https://github.com/agkozak/zhooks>
+        . "${pkgsUnstable.zsh-zhooks}/share/zsh/zhooks/zhooks.plugin.zsh"
       ''
       # starship, zoxide, skim, direnv, aliases will follow
 
       ''
         # EDITOR가 vi 이여도, ^A, ^E 같은 emacs 키는 사용할 수 있게 설정
         # https://github.com/simnalamburt/.dotfiles/blob/997d482/.zshrc
-        if (( $+commands[vim] )); then
-          bindkey '^A' beginning-of-line
-          bindkey '^E' end-of-line
-        elif (( $+commands[nvim] )); then
+        if (( $+commands[vim] )) || (( $+commands[nvim] )); then
           bindkey '^A' beginning-of-line
           bindkey '^E' end-of-line
         fi
@@ -77,6 +81,5 @@ in {
       share = true;
       # use atuin as history manager
     };
-    dotDir = ".config/zsh";
   };
 }
---
+++ hm-modules/generic-home/programs/00-shell/zsh/profile.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  programs.zsh = {
+    # .zprofle
+    profileExtra = lib.mkOrder 1 ''
+      [ -n "$__PROFILE_SOURCED" ] && return
+      __PROFILE_SOURCED=1
+    '';
+
+    initExtraFirst = lib.mkOrder 1 ''
+      [ -z "$__PROFILE_SOURCED" ] &&
+        [ -f "$HOME/${config.programs.zsh.dotDir}/.zprofile" ] &&
+        . "$HOME/${config.programs.zsh.dotDir}/.zprofile"
+    '';
+  };
+}
