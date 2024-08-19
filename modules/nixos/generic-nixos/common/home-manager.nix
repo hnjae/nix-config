7d168337ca50eea17a8f468184c7082a8ef24e5b
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-08-19 22:27:16 +0900

    feat: use home-manager as NixOS module

--- modules/nixos/generic-nixos/common/home-manager.nix
+++ modules/nixos/generic-nixos/common/home-manager.nix
@@ -1,18 +1,37 @@
 {
-  config,
   inputs,
+  pkgs,
   lib,
   ...
 }: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
   ];
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = false;
-  home-manager.extraSpecialArgs = lib.mkOverride 100 {
-    pkgsUnstable = import inputs.nixpkgs-unstable {
-      system = inputs.flake-utils.lib.system.x86_64-linux;
-      config.allowUnfree = true;
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = false;
+    backupFileExtension = "backup";
+    sharedModules = [
+      inputs.impermanence.nixosModules.home-manager.impermanence
+      inputs.nix-flatpak.homeManagerModules.nix-flatpak
+      inputs.base16.homeManagerModule
+      inputs.nix-index-database.hmModules.nix-index
+      inputs.nix-web-app.homeManagerModules.default
+
+      (import ../../../home-manager/default-app)
+      (import ../../../home-manager/python)
+      (import ../../../home-manager/stateful)
+      (import ../../../home-manager/generic-home)
+    ];
+    extraSpecialArgs = {
+      pkgsUnstable = import inputs.nixpkgs-unstable {
+        inherit (pkgs.stdenv) system;
+        config.allowUnfree = pkgs.config.allowUnfree;
+        overlays = [
+          inputs.rust-overlay.overlays.default
+        ];
+      };
     };
   };
 }
