e36cefb8ea4e142da122efaef7f862db4aa6f974
KIM Hyunjae <hnjae@outlook.com>
2024-08-30 18:02:26 +0900

    feat(plasma): web-app 윈도우의 시작 크기 지정

--- modules/home-manager/plasma6/system-settings/apps-and-windows/window-management/window-rules/default.nix
+++ modules/home-manager/plasma6/system-settings/apps-and-windows/window-management/window-rules/default.nix
@@ -1,17 +1,15 @@
-{...}: {
+_: {
   # README: 말도 안되게 작아지는 앱들 한정해서 설정할 것.
   imports = [
-    ./picture-in-picture.nix
-    ./password-promt.nix
-    ./minsize.nix
-    ./minsize-thunderbird.nix
-    ./force-border.nix
-
-    ./show-in-all-activities.nix
-    ./show-in-all-activities-and-desktop.nix
-
     ./above-all-other-windows.nix
-
     ./chromium.nix
+    ./force-border.nix
+    ./minsize-thunderbird.nix
+    ./minsize.nix
+    ./password-promt.nix
+    ./picture-in-picture.nix
+    ./show-in-all-activities-and-desktop.nix
+    ./show-in-all-activities.nix
+    ./web-app.nix
   ];
 }
