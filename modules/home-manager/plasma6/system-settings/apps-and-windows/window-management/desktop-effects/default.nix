83969913f2e52102cf07dbd5f756c1815e83e38b
KIM Hyunjae <hnjae@outlook.com>
2024-09-13 01:06:42 +0900

    various updates

--- modules/home-manager/plasma6/system-settings/apps-and-windows/window-management/desktop-effects/default.nix
+++ modules/home-manager/plasma6/system-settings/apps-and-windows/window-management/desktop-effects/default.nix
@@ -6,13 +6,16 @@
 
   programs.plasma.configFile."kwinrc" = {
     "Effect-slide"."SlideBackground" = false;
-    "Effect-overview"."OrganizedGrid" = false;
-
+    "Effect-overview" = {
+      "OrganizedGrid" = true; # orgnaize window in grids
+      "LayoutMode" = 0; # Closest (not Natural)
+    };
     "Plugins" = {
       "dimscreenEnabled" = true; # dim screen for Administrator
+
       # desktop switch animation
       "fadedesktopEnabled" = false;
-      "slideEnabled" = false;
+      "slideEnabled" = true;
     };
   };
 }
