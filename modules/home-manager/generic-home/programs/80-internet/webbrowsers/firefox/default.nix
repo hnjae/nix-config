cb3d28187b8696a2b3a36cff5f47e27b5b73c4e0
KIM Hyunjae <hnjae@outlook.com>
2024-09-16 03:49:38 +0900

    feat(generic-home): add extensions to firefox profile

--- modules/home-manager/generic-home/programs/80-internet/webbrowsers/firefox/default.nix
+++ modules/home-manager/generic-home/programs/80-internet/webbrowsers/firefox/default.nix
@@ -30,6 +30,17 @@ in {
     package = pkgs.firefox;
     nativeMessagingHosts = [];
     policies = {};
-    profiles.home = {};
+    profiles.home = {
+      extensions = with pkgs; [
+        nur.repos.rycee.firefox-addons.darkreader
+        nur.repos.rycee.firefox-addons.vimium-c
+        nur.repos.rycee.firefox-addons.leechblock-ng
+
+        # https://www.privacytools.io/privacy-browser-addons
+        nur.repos.rycee.firefox-addons.ublock-origin
+        nur.repos.rycee.firefox-addons.localcdn
+        nur.repos.rycee.firefox-addons.clearurls
+      ];
+    };
   };
 }
