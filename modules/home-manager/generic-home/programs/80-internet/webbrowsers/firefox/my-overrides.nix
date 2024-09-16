6eabe21e4601cd902aa5682810c03eb0531f9bfa
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-09-16 16:05:04 +0900

    feat(generic-home): remove unfinished firefox-ui-fix

--- modules/home-manager/generic-home/programs/80-internet/webbrowsers/firefox/my-overrides.nix
+++ modules/home-manager/generic-home/programs/80-internet/webbrowsers/firefox/my-overrides.nix
@@ -1,5 +1,6 @@
 {lib, ...}: {
   programs.firefox.profiles.home.extraConfig = lib.mkAfter ''
     user_pref("extensions.pocket.enabled", true);
+    user_pref("browser.compactmode.show", true);
   '';
 }
