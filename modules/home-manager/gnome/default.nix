766b8571e6016f6f4cde96eb0beee10fdaba523a
KIM Hyunjae <hnjae@outlook.com>
2024-11-15 11:52:03 +0900

    refactor(gnome): relocate some dconf settings to nixosModule

--- modules/home-manager/gnome/default.nix
+++ modules/home-manager/gnome/default.nix
@@ -1,17 +1,5 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{pkgs, ...}: {
   home.packages = with pkgs; [
     dconf2nix
   ];
-
-  # TODO: add followings <2024-11-15>
-  # gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
-  dconf.settings = with lib.hm.gvariant; {
-    "org/gnome/mutter" = {
-      experimental-features = ["scale-monitor-framebuffer"];
-    };
-  };
 }
