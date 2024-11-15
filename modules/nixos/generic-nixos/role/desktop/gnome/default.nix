d2ad8f030ef663ced1d176e2136ca55b003aa6e2
KIM Hyunjae <hnjae@outlook.com>
2024-11-15 13:09:50 +0900

    refactor(gnome): separate ime-related code into new file

--- modules/nixos/generic-nixos/role/desktop/gnome/default.nix
+++ modules/nixos/generic-nixos/role/desktop/gnome/default.nix
@@ -4,23 +4,10 @@
   lib,
   ...
 }: {
+  imports = [
+    ./ime.nix
+  ];
   config = lib.mkIf (config.generic-nixos.role == "desktop") {
-    # IME
-    i18n.inputMethod = {
-      enable = true;
-      type = "fcitx5";
-      fcitx5 = {
-        plasma6Support = false;
-        addons = with pkgs; [
-          fcitx5-gtk
-          fcitx5-mozc
-          fcitx5-hangul
-          # fcitx5-m17n
-          fcitx5-lua
-        ];
-      };
-    };
-
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome = {
@@ -32,25 +19,7 @@
     };
 
     home-manager.sharedModules = [
-      (import ../../../../home-manager/gnome)
-      {
-        # hide fcitx5-migrator desktop entry
-        xdg.desktopEntries."org.fcitx.fcitx5-migrator" = {
-          name = "fcitx5-migration-wizard";
-          comment = "this should not be displayed";
-          exec = ":";
-          type = "Application";
-          noDisplay = true;
-        };
-        # hide fcitx5 desktop entry
-        xdg.desktopEntries."org.fcitx.Fcitx5" = {
-          name = "fcitx5";
-          comment = "this should not be displayed";
-          exec = ":";
-          type = "Application";
-          noDisplay = true;
-        };
-      }
+      (import ../../../../../home-manager/gnome)
       {
         # with lib.hm.gvariant;
         dconf.settings = {
@@ -69,13 +38,6 @@
           };
         };
       }
-      {
-        dconf.settings = {
-          "org/gnome/shell".enabled-extensions = [
-            "kimpanel@kde.org"
-          ];
-        };
-      }
     ];
 
     services.gnome = {
@@ -87,7 +49,6 @@
     environment.defaultPackages = with pkgs.gnomeExtensions; [
       paperwm
       run-or-raise
-      kimpanel # to use with fcitx5
     ];
     environment.systemPackages = with pkgs; [
       nautilus
---
+++ modules/nixos/generic-nixos/role/desktop/gnome/ime.nix
@@ -0,0 +1,57 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  config =
+    lib.mkIf (
+      config.services.xserver.desktopManager.gnome.enable
+    ) {
+      # IME
+      i18n.inputMethod = {
+        enable = true;
+        type = "fcitx5";
+        fcitx5 = {
+          plasma6Support = false;
+          addons = with pkgs; [
+            fcitx5-gtk
+            fcitx5-mozc
+            fcitx5-hangul
+            # fcitx5-m17n
+            fcitx5-lua
+          ];
+        };
+      };
+      home-manager.sharedModules = [
+        {
+          # hide fcitx5-migrator desktop entry
+          xdg.desktopEntries."org.fcitx.fcitx5-migrator" = {
+            name = "fcitx5-migration-wizard";
+            comment = "this should not be displayed";
+            exec = ":";
+            type = "Application";
+            noDisplay = true;
+          };
+          # hide fcitx5 desktop entry
+          xdg.desktopEntries."org.fcitx.Fcitx5" = {
+            name = "fcitx5";
+            comment = "this should not be displayed";
+            exec = ":";
+            type = "Application";
+            noDisplay = true;
+          };
+        }
+        {
+          dconf.settings = {
+            "org/gnome/shell".enabled-extensions = [
+              "kimpanel@kde.org"
+            ];
+          };
+        }
+      ];
+      environment.systemPackages = with pkgs.gnomeExtensions; [
+        kimpanel # to use with fcitx5
+      ];
+    };
+}
