f550727624217766efa451faec4e20c3e7e66307
KIM Hyunjae <hnjae@outlook.com>
2024-11-15 13:05:07 +0900

    feat(gnome): enable kimpanel extension

--- modules/nixos/generic-nixos/role/desktop/gnome.nix
+++ modules/nixos/generic-nixos/role/desktop/gnome.nix
@@ -69,6 +69,13 @@
           };
         };
       }
+      {
+        dconf.settings = {
+          "org/gnome/shell".enabled-extensions = [
+            "kimpanel@kde.org"
+          ];
+        };
+      }
     ];
 
     services.gnome = {
