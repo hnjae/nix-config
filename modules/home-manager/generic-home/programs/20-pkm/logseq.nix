ca7010a01a102522cb9ba163c5236996e35fef90
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-11-15 12:34:23 +0900

    feat(logseq): disable wayland
    Logseq doesn't support text-input-v3 yet. Since text-input-v1 is not available on gnome, xwayland is used as a workaround.

--- modules/home-manager/generic-home/programs/20-pkm/logseq.nix
+++ modules/home-manager/generic-home/programs/20-pkm/logseq.nix
@@ -1,3 +1,10 @@
+/*
+NOTE:  <2024-11-15>
+
+Logesq 은 현재 electron 28 을 사용하고 있으며, 이는 text-input-v3 를 아직 지원하지 않는다.
+
+electron 33+ 를 사용해야함.
+*/
 {
   config,
   lib,
@@ -28,7 +35,8 @@ in {
     services.flatpak.overrides."${appId}" = {
       # Environment = { "GTK_IM_MODULE" = "xim"; };
       Context = {
-        sockets = ["!x11"];
+        # sockets = ["!x11"];
+        sockets = ["!wayland"];
         # for git support
         # filesystems = [
         #   "~/.ssh"
@@ -36,41 +44,24 @@ in {
         # ];
       };
     };
-    xdg.dataFile."applications/com.logseq.Logseq.desktop" = let
-      flags = builtins.concatStringsSep " " electronFlags;
-    in {
-      enable = true;
-      text = ''
-        [Desktop Entry]
-        Name=Logseq Desktop
-        Exec=flatpak run --branch=stable --arch=x86_64 --command=run.sh --file-forwarding com.logseq.Logseq @@u %U @@ ${flags}
-        Terminal=false
-        Type=Application
-        Icon=com.logseq.Logseq
-        StartupWMClass=Logseq
-        Comment=Custom desktop entries.
-        MimeType=x-scheme-handler/logseq;
-        Categories=Utility;Office;
-        NoDisplay=false
-        X-Flatpak=com.logseq.Logseq
-      '';
-    };
-    # xdg.desktopEntries."com.logseq.Logseq" = {
-    #   name = "Logseq";
-    #   icon = "com.logseq.Logseq";
-    #   startupWMClass = "Logseq";
-    #   comment = "Run app with custom desktop entries";
-    #   exec = "flatpak run --branch=stable --arch=x86_64 --command=run.sh --file-forwarding com.logseq.Logseq @@u %U @@";
-    #   terminal = false;
-    #   type = "Application";
-    #   startupNotify = true;
-    #   categories = [
-    #     "Utility"
-    #     "Office"
-    #   ];
-    #   mimeType = [
-    #     "x-scheme-handler/logseq"
-    #   ];
+    # xdg.dataFile."applications/com.logseq.Logseq.desktop" = let
+    #   flags = builtins.concatStringsSep " " electronFlags;
+    # in {
+    #   enable = true;
+    #   text = ''
+    #     [Desktop Entry]
+    #     Name=Logseq Desktop
+    #     Exec=flatpak run --branch=stable --arch=x86_64 --command=run.sh --file-forwarding com.logseq.Logseq @@u %U @@ ${flags}
+    #     Terminal=false
+    #     Type=Application
+    #     Icon=com.logseq.Logseq
+    #     StartupWMClass=Logseq
+    #     Comment=Custom desktop entries.
+    #     MimeType=x-scheme-handler/logseq;
+    #     Categories=Utility;Office;
+    #     NoDisplay=false
+    #     X-Flatpak=com.logseq.Logseq
+    #   '';
     # };
   };
 }
