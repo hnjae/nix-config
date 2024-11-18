ef507ee532ec065474e2f9afb0707386531553bc
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-11-18 22:24:02 +0900

    feat(gnome): declarative fcitx5 setup

--- modules/nixos/generic-nixos/role/desktop/gnome/ime.nix
+++ modules/nixos/generic-nixos/role/desktop/gnome/ime.nix
@@ -16,11 +16,58 @@
           plasma6Support = false;
           addons = with pkgs; [
             fcitx5-gtk
-            fcitx5-mozc
+            fcitx5-mozc-ut
             fcitx5-hangul
             # fcitx5-m17n
             fcitx5-lua
           ];
+          settings = {
+            # config
+            globalOptions = {
+              "Hotkey/TriggerKeys" = {
+                "0" = "Zenkaku_Hankaku";
+                "1" = "Hangul";
+              };
+              "Hotkey/EnumerateGroupForwardKeys" = {
+              };
+              "Hotkey/EnumerateGroupBackwardKeys" = {
+                "0" = "Control+Shift+space";
+              };
+            };
+            # profile
+            inputMethod = {
+              "Groups/0" = {
+                Name = "ja";
+                "Default Layout" = "us-colemak_dh";
+                DefaultIM = "mozc";
+              };
+              "Groups/0/Items/0" = {
+                Name = "keyboard-use-colemak_dh";
+                Layout = "";
+              };
+              "Groups/0/Items/1" = {
+                Name = "mozc";
+                Layout = "";
+              };
+              "Groups/1" = {
+                Name = "ko";
+                "Default Layout" = "us-colemak_dh";
+                DefaultIM = "hangul";
+              };
+              "Groups/1/Items/0" = {
+                Name = "keyboard-use-colemak_dh";
+                Layout = "";
+              };
+              "Groups/1/Items/1" = {
+                Name = "hangul";
+                Layout = "us";
+              };
+              "GroupOrder" = {
+                "0" = "ko";
+                "1" = "ja";
+              };
+            };
+          };
         };
       };
       home-manager.sharedModules = [
@@ -54,6 +101,20 @@
             };
           };
         }
+        # {
+        #   stateful.nodes = [
+        #     {
+        #       path = "${config.xdg.configHome}/mozc";
+        #       mode = "700";
+        #       type = "dir";
+        #     }
+        #     {
+        #       path = "${config.xdg.configHome}/fcitx5";
+        #       mode = "700";
+        #       type = "dir";
+        #     }
+        #   ];
+        # }
       ];
       environment.systemPackages = with pkgs.gnomeExtensions; [
         kimpanel # to use with fcitx5
