af6d522a95412312bba4dcb5cecd8a12ed3c5064
KIM Hyunjae <hnjae@outlook.com>
2024-11-25 12:24:38 +0900

    feat(shell): add dircolors to generate LS_COLORS

--- modules/home-manager/generic-home/programs/00-shell/programs/default.nix
+++ modules/home-manager/generic-home/programs/00-shell/programs/default.nix
@@ -1,14 +1,15 @@
 # NOTE: enableBashIntegration 같은 것이 있는 옵션들 <2024-02-13>
 {...}: {
   imports = [
-    ./starship.nix
-    ./greeting.nix
-    ./zoxide.nix
-    ./mc.nix
-    ./chpwd.nix
     ./any-nix-shell.nix
     ./atuin.nix
     ./base24
+    ./chpwd.nix
+    ./dircolors.nix
     ./direnv.nix
+    ./greeting.nix
+    ./mc.nix
+    ./starship.nix
+    ./zoxide.nix
   ];
 }
