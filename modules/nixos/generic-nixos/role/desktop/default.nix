e96339f77d39b9d3282cc8dea68246449748ead1
KIM Hyunjae <hnjae@outlook.com>
2024-12-14 00:01:02 +0900

    feat(generic-nixos): make capslock to keep send backspace key events on x11

--- modules/nixos/generic-nixos/role/desktop/default.nix
+++ modules/nixos/generic-nixos/role/desktop/default.nix
@@ -12,4 +12,22 @@
     ./printing.nix
     ./upower.nix
   ];
+
+  # NOTE: https://gitlab.freedesktop.org/xorg/xserver/-/issues/1384 <2024-12-12>
+  # X11 이나 xwayland 에서 caps:backspace 가 활성화되어 있을시, caps 가 backspace 를 keep-sending 하지 않는 문제 수정
+  # xset r 66
+  home-manager.sharedModules = [
+    {
+      xdg.configFile."autostart/fix-xwayland-ctrl-backspace.desktop" = {
+        enable = true;
+        text = ''
+          [Desktop Entry]
+          Exec=sh -c 'sleep 0.2 && xset r 66'
+          Name=fix-xwayland-ctrl-backspace
+          Terminal=false
+          Type=Application
+        '';
+      };
+    }
+  ];
 }
