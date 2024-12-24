81340075f105b7f8476791084920699ae5e63580
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-12-24 18:20:14 +0900

    various updates

--- modules/home-manager/generic-home/programs/80-multimedia/qimgv/default.nix
+++ modules/home-manager/generic-home/programs/80-multimedia/qimgv/default.nix
@@ -9,7 +9,7 @@ in {
   config = lib.mkIf (pkgs.stdenv.isLinux && genericHomeCfg.isDesktop) {
     home.packages = [
       (pkgs.qimgv-git.override {
-        mpv-unwrapped = (import ../mpv/package.nix) {inherit config pkgs;};
+        # mpv-unwrapped = (import ../mpv/package.nix) {inherit config pkgs;};
       })
     ];
     default-app.image = lib.mkDefault "qimgv";
