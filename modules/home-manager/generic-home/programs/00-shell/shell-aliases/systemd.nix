77aaf92b4964294c1c217ab4f71a1b42041d67fb
KIM Hyunjae <hnjae@outlook.com>
2025-01-04 20:26:17 +0900

    feat(shell): add additional systemd command aliases

--- modules/home-manager/generic-home/programs/00-shell/shell-aliases/systemd.nix
+++ modules/home-manager/generic-home/programs/00-shell/shell-aliases/systemd.nix
@@ -4,8 +4,26 @@
   ...
 }: let
   aliases = {
-    sys = "systemctl";
-    sysu = "systemctl --user";
+    sc = "systemctl";
+    scu = "systemctl --user";
+
+    scst = "systemctl status";
+    scust = "systemctl --user status";
+
+    sclst = "systemctl list-timers";
+    sculst = "systemctl --user list-timers";
+
+    sc-failed = "systemctl --failed";
+    sc-ufailed = "systemctl --user --failed";
+
+    sc-restart = "systemctl --restart";
+    scu-restart = "systemctl --user --restart";
+
+    jc = "journalctl";
+    jcu = "journalctl --user";
+
+    jcunit = "journalctl -xeu";
+    jcuunit = "journalctl --user -xeu";
   };
 in {
   config = lib.mkIf (pkgs.stdenv.isLinux) {
