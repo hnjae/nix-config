f7ee0680f2b0b31e97ef20dcea5704dc211ce50b
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-01-06 10:59:28 +0900

    refactor(generic-nixos): move single-line modules to default.nix

--- modules/nixos/generic-nixos/role/desktop/documentation.nix
+++
@@ -1,8 +0,0 @@
-{
-  config,
-  lib,
-  ...
-}: {
-  # environment.systemPackages = [ pkgs.man-pages pkgs.man-pages-posix ];
-  documentation.dev.enable = lib.mkOverride 999 (config.generic-nixos.role == "desktop");
-}
