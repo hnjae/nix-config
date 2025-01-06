88c0c350e3782934fae76a20f7d8491605ace311
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-01-06 11:08:08 +0900

    refactor(generic-nixos): move hardware settings related to desktop role

--- modules/nixos/generic-nixos/core/hardware.nix
+++ modules/nixos/generic-nixos/core/hardware.nix
@@ -16,21 +16,4 @@ in {
   # NOTE: these firmware will be loaded if kernel requested <2023-10-03>
   hardware.enableAllFirmware = pkgs.config.allowUnfree;
   hardware.enableRedistributableFirmware = pkgs.config.allowUnfree;
-
-  services.libinput = {
-    enable = mkOverride 999 true;
-    # mouse.accelProfile = "flat";
-  };
-
-  # sound
-  security.rtkit.enable = mkOverride 999 isDesktop;
-  hardware.pulseaudio.enable = false; # use pipewire
-
-  # opengl
-  hardware.graphics = {
-    enable = mkOverride 999 isDesktop;
-    enable32Bit = mkOverride 999 isDesktop;
-  };
-
-  hardware.i2c.enable = mkOverride 999 isDesktop;
 }
