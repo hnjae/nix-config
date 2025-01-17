6ca9d3652bf1239120adec0f0469c628f0337c74
KIM Hyunjae <hnjae@outlook.com>
2025-01-17 13:04:38 +0900

    chore: remove unused packages and files

--- packages/flake-module.nix
+++ packages/flake-module.nix
@@ -21,7 +21,6 @@ in {
 
     packages = {
       nixvim = (import ./tools/nixvim) inputs.nixvim pkgs;
-      vim-declared = (import ./tools/vim-declared) pkgs;
       fonts-dmca-sans-serif = pkgs.callPackage ./fonts/fonts-dmca-sans-serif {};
       fonts-plangothic = pkgs.callPackage ./fonts/fonts-plangothic {};
       fonts-ridibatang = pkgs.callPackage ./fonts/fonts-ridibatang {};
@@ -47,38 +46,8 @@ in {
       ${system} = {
         # tools
         cavif-rs = pkgs.callPackage ./tools/cavif-rs {};
-        lf-sixel = pkgs.callPackage ./tools/lf-sixel {};
         xdg-terminal-exec = pkgs.callPackage ./tools/xdg-terminal-exec {};
         qimgv-git = pkgs.kdePackages.callPackage ./tools/qimgv-git {};
-
-        # Plasma 6
-        compact-pager = pkgs.callPackage ./kde/compact-pager {};
-        application-title-bar = pkgs.callPackage ./kde/application-title-bar {};
-
-        # Plasma 5
-        plasma-applet-active-window-control =
-          pkgs.libsForQt5.callPackage
-          ./kde/plasma-applet-active-window-control
-          {};
-        kwin-script-always-open-on =
-          pkgs.libsForQt5.callPackage
-          ./kde/kwin-script-always-open-on {};
-        # koi =
-        #   pkgs.libsForQt5.callPackage ./kde/koi {};
-
-        # SDDM
-        sddm-theme-slice = pkgs.callPackage ./themes/sddm-theme-slice {};
-        sddm-theme-corners = pkgs.callPackage ./themes/sddm-theme-corners {};
-        sddm-theme-sugar-dark = pkgs.callPackage ./themes/sddm-theme-sugar-dark {};
-
-        # Wallpapers
-        wallpapers-whitesur = pkgs.callPackage ./themes/wallpapers-whitesur {};
-
-        # Plasma 5 Theme (아마도 Plasma6 랑 호환될듯?)
-        kde-theme-whitesur = pkgs.callPackage ./themes/kde-theme-whitesur {};
-        kde-theme-fluent = pkgs.callPackage ./themes/kde-theme-fluent {};
-        kde-theme-monterey = pkgs.callPackage ./themes/kde-theme-monterey {};
-        kde-theme-we10xos = pkgs.callPackage ./themes/kde-theme-we10xos {};
       };
     });
 }
