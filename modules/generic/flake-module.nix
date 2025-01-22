5a8d97a912ced1dd2bd86b50c10c9367fafac3f0
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-01-22 14:22:01 +0900

    refactor(flake-module): merge dependencies modules

--- modules/generic/flake-module.nix
+++ modules/generic/flake-module.nix
@@ -2,15 +2,11 @@
 nixosModules/homeManagerModules 에서 self, inputs 를 쓰면, 외부 리포지토리에서는 이 모듈을 정상적으로 사용할 수 없다.
 이들은 flake.nix 를 최종적으로 호출하는 곳에서 정의되는 변수이기 때문이다.
 */
-{
-  self,
-  inputs,
-  ...
-}: {
-  flake = {
-    homeManagerModules.generic-home = import ./homeManagerModule;
-    homeManagerModules._generic-home-deps = {
+{inputs, ...}: {
+  flake = rec {
+    homeManagerModules.generic-home = {
       imports = [
+        (import ./homeManagerModule)
         inputs.base16.homeManagerModule
         inputs.impermanence.nixosModules.home-manager.impermanence
         inputs.nix-flatpak.homeManagerModules.nix-flatpak
@@ -19,15 +15,11 @@ nixosModules/homeManagerModules 에서 self, inputs 를 쓰면, 외부 리포지
       ];
     };
 
-    nixosModules.generic-nixos = import ./nixosModule;
-    nixosModules._generic-nixos-deps = {
+    nixosModules.generic-nixos = {
       imports = [
-        self.nixosModules.nix-gc-system-generations
-        self.nixosModules.nix-store-gc
-      ];
-    };
-    nixosModules._configure-generic-home-for-nixos = {
-      imports = [
+        (import ./nixosModule)
+        (import ../nixosModules/services/nix-gc-system-generations)
+        (import ../nixosModules/services/nix-store-gc)
         inputs.home-manager.nixosModules.home-manager
         (
           {
@@ -43,8 +35,7 @@ nixosModules/homeManagerModules 에서 self, inputs 를 쓰면, 외부 리포지
                 useUserPackages = false;
                 backupFileExtension = "backup";
                 sharedModules = [
-                  self.homeManagerModules.generic-home
-                  self.homeManagerModules._generic-home-deps
+                  homeManagerModules.generic-home
                   {
                     nixpkgs.overlays = [self.overlays.default];
                   }
