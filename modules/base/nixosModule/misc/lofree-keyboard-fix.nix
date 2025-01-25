7df385c1e0a274f7aad371d38d694c76dddaf39b
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-01-25 16:42:25 +0900

    feat(base-nixos): fix lofree keyboard

---
+++ modules/base/nixosModule/misc/lofree-keyboard-fix.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  config,
+  ...
+}:
+{
+  config = lib.mkIf (config.base-nixos.role == "baremetal") {
+    # for lofree flow keyboard (and apple keyboard (maybe))
+
+    # NOTE: udev 로는 bluetooth 연결의 경우를 커버하지 못함. <2024-04-29>
+    # services.udev.extraRules = ''
+    #   ACTION=="add", ATTRS{product}=="Flow84@Lofree", ATTR{idProduct}=="024f" ATTR{idVendor}=="05ac", RUN+="${pkgs.dash}/bin/dash -c 'echo 2 > /sys/module/hid_apple/parameters/fnmode'"
+    # '';
+
+    boot.extraModprobeConfig = ''
+      options hid_apple fnmode=2
+    '';
+  };
+}
