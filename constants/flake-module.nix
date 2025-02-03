87dc9e8e9bc8c025f1feaf6f33eb410a704bafd2
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-02-03 20:27:20 +0900

    chore: fix git-crypt rule

--- constants/flake-module.nix
+++ constants/flake-module.nix
@@ -2,6 +2,6 @@ _: {
   flake.constants = {
     # my ssh public key
     homeSshPublic = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMJzpwZFnwPxTF4TU7IX5AI+Nwpu9VvjI4A9Jlh3P0pu";
-    home = import ./home.nix;
+    home = import ./home-encrypted.nix;
   };
 }
