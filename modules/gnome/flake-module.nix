34a6d1cb750847a87ed8bf51890f5ae8514aa823
KIM Hyunjae <hnjae@outlook.com>
2025-02-03 23:53:41 +0900

    refactor: rename directories

--- modules/gnome/flake-module.nix
+++ modules/gnome/flake-module.nix
@@ -1,11 +1,11 @@
 _: {
   flake = {
-    nixosModules.gnome = import ./nixosModule;
+    nixosModules.gnome = import ./nixos-module;
     /*
       Module Dependency:
         * home-manager
         * base-home
     */
-    homeManagerModules.gnome = import ./homeManagerModule;
+    homeManagerModules.gnome = import ./hm-module;
   };
 }
