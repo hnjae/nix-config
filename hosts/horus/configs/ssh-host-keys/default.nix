e6a66bd3eb6e97e4b3b9e021698deb3c0e24da20
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-02-06 18:16:19 +0900

    fix(horus): correct typo in ssh host keys configuration

--- hosts/horus/configs/ssh-host-keys/default.nix
+++ hosts/horus/configs/ssh-host-keys/default.nix
@@ -1,14 +1,20 @@
-{ config, ... }:
+{ config, self, ... }:
 {
   sops.secrets.ssh-host-key-prv = {
     format = "binary";
     sopsFile = ./secrets/horus.ssh_host_ed25519_key;
+    path = "/etc/ssh/ssh_host_ed25519_key";
     mode = "0400";
+  };
+
+  environment.etc."ssh/ssh_host_ed25519_key.pub" = {
+    text = self.constants.hosts.horus.sshPublicKey;
+    mode = "0644";
   };
 
   services.openssh.hostKeys = [
     {
-      path = config.sops.secrets.ddclient-config.path;
+      path = config.sops.secrets.ssh-host-key-prv.path;
       type = "ed25519";
     }
     # {
