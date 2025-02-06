93c2394642b9a8b878eed880090e4923b74f237c
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-02-06 18:03:56 +0900

    fix(base-home): prevent ghostty installation on non-desktop systems

--- modules/profiles/base-home/programs/80-utilities/50-terminal-emulators/ghostty/default.nix
+++ modules/profiles/base-home/programs/80-utilities/50-terminal-emulators/ghostty/default.nix
@@ -1,31 +1,41 @@
-{ pkgsUnstable, ... }:
 {
-  home.packages = [ pkgsUnstable.ghostty-tip ];
-  default-app.fromApps = [ "com.mitchellh.ghostty" ];
+  config,
+  lib,
+  pkgsUnstable,
+  ...
+}:
+let
+  baseHomeCfg = config.base-home;
+in
+{
+  config = lib.mkIf baseHomeCfg.isDesktop {
+    home.packages = [ pkgsUnstable.ghostty-tip ];
+    default-app.fromApps = [ "com.mitchellh.ghostty" ];
 
-  # NOTE: <2024-12-30>
-  # https://github.com/ghostty-org/ghostty/discussions/3628
-  xdg.dataFile."applications/com.mitchellh.ghostty.desktop".text = ''
-    [Desktop Entry]
-    Name=Ghostty
-    Type=Application
-    Comment=A terminal emulator
-    Exec=env GTK_IM_MODULE="wayland" ghostty
-    Icon=com.mitchellh.ghostty
-    Categories=System;TerminalEmulator;
-    Keywords=terminal;tty;pty;
-    StartupNotify=true
-    Terminal=false
-    Actions=new-window;
-    X-GNOME-UsesNotifications=true
-    X-TerminalArgExec=-e
-    X-TerminalArgTitle=--title=
-    X-TerminalArgAppId=--class=
-    X-TerminalArgDir=--working-directory=
-    X-TerminalArgHold=--wait-after-command
+    # NOTE: <2024-12-30>
+    # https://github.com/ghostty-org/ghostty/discussions/3628
+    xdg.dataFile."applications/com.mitchellh.ghostty.desktop".text = ''
+      [Desktop Entry]
+      Name=Ghostty
+      Type=Application
+      Comment=A terminal emulator
+      Exec=env GTK_IM_MODULE="wayland" ghostty
+      Icon=com.mitchellh.ghostty
+      Categories=System;TerminalEmulator;
+      Keywords=terminal;tty;pty;
+      StartupNotify=true
+      Terminal=false
+      Actions=new-window;
+      X-GNOME-UsesNotifications=true
+      X-TerminalArgExec=-e
+      X-TerminalArgTitle=--title=
+      X-TerminalArgAppId=--class=
+      X-TerminalArgDir=--working-directory=
+      X-TerminalArgHold=--wait-after-command
 
-    [Desktop Action new-window]
-    Name=New Window
-    Exec=env GTK_IM_MODULE="wayland" ghostty
-  '';
+      [Desktop Action new-window]
+      Name=New Window
+      Exec=env GTK_IM_MODULE="wayland" ghostty
+    '';
+  };
 }
