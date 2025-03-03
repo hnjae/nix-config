5d8d07e532eb48d4822c742471df0342668c64c2
KIM Hyunjae <hnjae@outlook.com>
2025-03-03 19:18:51 +0900

    feat(isis): prevent restic backups on metered connections or VPN

--- hosts/isis/services/restic-backup/default.nix
+++ hosts/isis/services/restic-backup/default.nix
@@ -4,7 +4,11 @@
     - `/secrets/rclone.conf` 에 적절한 rclone 설정을 넣어두어야 한다.
     - `rclone.conf` 의 access_token 이 업데이트 될수 있어야 하므로, sops 로 모듈에 번들하지 않는다.
 
-  # TODO: networkmanager 에서 metered connection 인지 물어보기. <2025-03-03>
+    다음의 환경에서는 백업이 수행되지 않는다.
+      - 전원 연결이 되지 않은 상태
+      - VPN 에 연결되어 있고, VPN 을 통해 인터넷에 연결된 상태
+      - onedrive.live.com 에 접속할 수 없는 상태
+      - metered network 에 연결되어 있는 상태
 */
 {
   config,
@@ -13,123 +17,170 @@
   ...
 }:
 let
-  envResticOnedrive = pkgs.writeText "env-restic-onedrive" (
-    lib.generators.toKeyValue { } {
-      RESTIC_COMPRESSION = "auto";
-      RESTIC_PACK_SIZE = 128;
-      GOGC = "off";
-      GOMEMLIMIT = 4 * 1024 * 1024 * 1024; # 4GiB
-      RCLONE_BWLIMIT = "4M"; # MiB/s
-    }
-  );
+  serviceName = "restic-off-site-backup";
+  paths = [
+    "/home/hnjae/Projects"
+    # "/home/hnjae/Pictures"
+    # "/home/hnjae/Documents"
+    # "/home/hnjae/Library"
+  ];
+  exclude = [
+    # Linux
+    ".directory" # KDE
+    ".thumbnails" # KDE (maybe)
+    ".Trash-*"
+    ".nfs*"
+    ".fuse_hidden*"
 
-  offSiteBackupName = "off-site";
+    # macOS
+    ".DS_Store"
+    "._*" # thumbnails
+
+    # MS Windows
+    "Thumbs.db"
+    "Desktop.ini"
+    "desktop.ini"
+    "$RECYCLE.BIN"
+
+    # Misc
+    ".localized"
+    ".cache"
+
+    # temporary files
+    "*.parts"
+    ".direnv" # nix-flake
+
+    # vscode
+    ".vscode-server"
+
+    # vim
+    "tags"
+    "*.swp"
+    "*~"
+
+    # shell related
+    "fish_variables"
+    "*.zcompdump"
+    ".zsh_history"
+
+    # python related
+    ".venv"
+    "__pycache__"
+    ".pyc"
+
+    # python tools
+    ".ropeproject"
+    ".mypy_cache"
+    ".ruff_cache"
+    ".pyre"
+    "dist"
+
+    # nodejs related
+    "node_modules"
+    "dist"
+
+    # logseq
+    "logseq/.recycle"
+    "logseq/bak"
+  ];
 in
 {
-  sops.secrets."restic-onedrive" = {
-    sopsFile = ./secrets/restic-onedrive;
+  sops.secrets."restic-onedrive-repo-password" = {
+    sopsFile = ./secrets/restic-onedrive-repo-password;
     format = "binary";
   };
 
-  # Overrides
-  systemd.services."restic-backups-${offSiteBackupName}" = {
+  systemd.services."${serviceName}" = {
+    environment = {
+      RESTIC_COMPRESSION = "auto";
+      RESTIC_PACK_SIZE = builtins.toString 128;
+      RESTIC_CACHE_DIR = "/var/cache/${serviceName}";
+      RESTIC_PASSWORD_FILE = config.sops.secrets."restic-onedrive-repo-password".path;
+      RESTIC_REPOSITORY = "rclone:onedrive:.restic";
+
+      # GOGC = "off";
+      # GOMEMLIMIT = 4 * 1024 * 1024 * 1024; # 4GiB
+
+      RCLONE_CONFIG = "/secrets/rclone.conf";
+      RCLONE_BWLIMIT = "4M"; # MiB/s
+    };
     serviceConfig = {
+      Type = "simple"; # TODO: 나중에 oneshot 으로 변경 <2025-03-03>
+
+      CacheDirectory = "${serviceName}";
+      RuntimeDirectory = "${serviceName}";
+      CacheDirectoryMode = "0700";
+      PrivateTmp = true;
+
       Nice = 19;
       CPUSchedulingPolicy = "idle";
       IOSchedulingClass = "idle";
+
       ExecCondition = [
-        "${pkgs.inetutils}/bin/ping -c 1 'https://onedrive.live.com"
+        (pkgs.writeScript "${serviceName}-check-vpn-route" ''
+          #!${pkgs.dash}/bin/dash
+
+          set -eu
+
+          PATH="${pkgs.iproute2}/bin:${pkgs.gnugrep}/bin"
+
+          if ip route show default | grep -E "dev (tun|ppp)" >/dev/null 2>&1; then
+            echo "Network is routed to VPN"
+            exit 1
+          fi
+
+          exit 0
+        '')
+
+        (lib.lists.optional (config.networking.networkmanager.enable) (
+          pkgs.writeScript "${serviceName}-check-metered-connection" (
+            lib.concatLines [
+              ''
+                #!${pkgs.nushell}/bin/nu
+
+                $env.PATH = [
+                  '${pkgs.networkmanager}/bin'
+                ]
+              ''
+              (builtins.readFile ./resources/check-metered.nu)
+            ]
+          )
+        ))
+
+        (pkgs.writeScript "${serviceName}-check-internet" ''
+          #!${pkgs.dash}/bin/dash
+
+          set -eu
+
+          PATH="${pkgs.inetutils}/bin"
+
+          if ! ping -c 1 'https://onedrive.live.com' >/dev/null 2>&1; then
+            echo "Cannot connect to onedrive.live.com"
+            exit 1
+          fi
+
+          exit 0
+        '')
       ];
-      # ExecCondition = pkgs.writeScript "${offSiteBackupName}-condition" ''
-      #   #!${pkgs.dash}/bin/dash
-      #
-      #   PATH="${pkgs.inetutils}/bin"
-      #
-      #   ping -c 1.1.1.1
-      # '';
+
+      ExecStart =
+        let
+          excludeFile = pkgs.writeText "${serviceName}-exclude-file" (lib.concatLines exclude);
+          filesFrom = pkgs.writeText "${serviceName}-files-from" (lib.concatLines paths);
+        in
+        (builtins.concatStringsSep " " [
+          "${pkgs.restic}/bin/restic"
+          "backup"
+          "--one-file-system"
+          "--exclude-file=${excludeFile}"
+          "--files-from=${filesFrom}"
+        ]);
     };
+
     unitConfig = {
       ConditionACPower = true;
-    };
-  };
-
-  services.restic.backups = {
-    "${offSiteBackupName}" = {
-      # user = "hnjae";
-      timerConfig = {
-        OnCalendar = "daily";
-        Persistent = true;
-      };
-      repository = "rclone:onedrive:.restic";
-      rcloneConfigFile = "/secrets/rclone.conf";
-      paths = [
-        "/home/hnjae/Projects"
-        # "/home/hnjae/Pictures"
-        # "/home/hnjae/Documents"
-        # "/home/hnjae/Library"
-      ];
-      environmentFile = "${envResticOnedrive}";
-      passwordFile = config.sops.secrets."restic-onedrive".path;
-      initialize = false;
-      inhibitsSleep = false;
-      extraBackupArgs = [
-        "--one-file-system"
-      ];
-      exclude = [
-        # Linux
-        ".directory" # KDE
-        ".thumbnails" # KDE (maybe)
-        ".Trash-*"
-        ".nfs*"
-        ".fuse_hidden*"
-
-        # macOS
-        ".DS_Store"
-        "._*" # thumbnails
-
-        # MS Windows
-        "Thumbs.db"
-        "Desktop.ini"
-        "desktop.ini"
-        "$RECYCLE.BIN"
-
-        # Misc
-        ".localized"
-        ".cache"
-
-        # temporary files
-        "*.parts"
-        ".direnv" # nix-flake
-
-        # vscode
-        ".vscode-server"
-
-        # vim
-        "tags"
-        "*.swp"
-        "*~"
-
-        # shell related
-        "fish_variables"
-        "*.zcompdump"
-        ".zsh_history"
-
-        # python related
-        ".venv"
-        "__pycache__"
-        ".pyc"
-
-        # python tools
-        ".ropeproject"
-        ".mypy_cache"
-        ".ruff_cache"
-        ".pyre"
-        "dist"
-
-        # nodejs related
-        "node_modules"
-        "dist"
-      ];
+      After = [ "network-online.target" ];
+      Wants = [ "network-online.target" ];
     };
   };
 }
