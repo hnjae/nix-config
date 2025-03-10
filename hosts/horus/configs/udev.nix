7eaf30fef2f049bc9afc9132dcfda2ad7fa434f4
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-03-10 19:57:28 +0900

    various updates

--- hosts/horus/configs/udev.nix
+++ hosts/horus/configs/udev.nix
@@ -10,10 +10,12 @@
   # of 30 minutes.
   # -S 251: 320min (5h 20m)
   # -S 245: 170min
+  # -S 244: 140min
   # -S 243: 110min
   # -S 242: 80min
 
+  # 243 에서 위로는 변경하지 말 것. 관련되서 하드코딩 되어 있다. <2025-03-10>
   services.udev.extraRules = ''
-    ACTION=="add|change", KERNEL=="sd[a-z]", ATTRS{queue/rotational}=="1", RUN+="${pkgs.hdparm}/sbin/hdparm -B 128 -S 244 /dev/%k"
+    ACTION=="add|change", KERNEL=="sd[a-z]", ATTRS{queue/rotational}=="1", RUN+="${pkgs.hdparm}/sbin/hdparm -B 128 -S 243 /dev/%k"
   '';
 }
