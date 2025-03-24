e66b51cb9be51309ed19011d522be17e031cee59
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-03-24 23:35:51 +0900

    isis: 2025-03-24T23:35:51+09:00

--- hosts/isis/hardware/bootloader.nix
+++ hosts/isis/hardware/bootloader.nix
@@ -24,6 +24,12 @@ in
         efiSysMountPoint = "/boot";
       };
     };
+
+    kernelParams = [
+      # 백라이트가 갑자기 꺼지는 문제 해결 (WIP; 테스트 안됨; 6.12.19; NixOS 24.11)
+      # https://forums.lenovo.com/t5/Other-Linux-Discussions/ThinkPad-T16-Gen-1-on-Linux-backlight-switches-off-once-in-a-while/m-p/5260463
+      "amdgpu.dcdebugmask=0x10" # turn off Panel-Self-Refresh (PSR)
+    ];
   };
 
   environment.systemPackages = [
