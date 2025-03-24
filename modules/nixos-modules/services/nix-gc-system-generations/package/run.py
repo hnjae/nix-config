0efc54cdcc77bba18e966d3f6406d563e183e747
KIM Hyunjae <hnjae@outlook.com>
2025-03-25 01:23:01 +0900

    feat(nix-gc-system-generations): improve date handling
    - 04시를 날짜 변경 기준으로 계산.

--- modules/nixos-modules/services/nix-gc-system-generations/package/run.py
+++ modules/nixos-modules/services/nix-gc-system-generations/package/run.py
@@ -3,17 +3,19 @@
 from __future__ import annotations
 
 import argparse
+import logging
 import subprocess
 import sys
 from collections import defaultdict
-from datetime import date, datetime, timedelta, timezone
+from datetime import datetime, timedelta
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, override
 
 if TYPE_CHECKING:
     from argparse import Namespace
-    from typing import Final
     from collections.abc import Iterable, Mapping
+    from datetime import date
+    from typing import Final
 
 # ruff: noqa: ANN204, D107, D105
 
@@ -28,40 +30,56 @@ PROFILE_PATH: Final = "/nix/var/nix/profiles"
 BOOTED_SYS_NIX_PATH: Final[Path] = Path("/run/booted-system").readlink()
 CURRENT_SYS_NIX_PATH: Final[Path] = Path("/run/current-system").readlink()
 
+DAY_ADJUST: Final = timedelta(hours=4)  # 04 시를 날짜 변경 기준으로 삼음
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s: %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S%z",
+)
+logger = logging.getLogger(__name__)
+
 
 class NixGeneration:
     def __init__(
         self, number: int, datetime_: datetime, *, is_current_profile: bool
     ):
-        self.number = number
-        self.profile_path = Path(PROFILE_PATH, f"system-{number}-link")
+        self.number: Final[int] = number
+        self.profile_path: Final[Path] = Path(
+            PROFILE_PATH, f"system-{number}-link"
+        )
 
-        self.nix_path = self.profile_path.readlink()
+        self.nix_path: Final[Path] = self.profile_path.readlink()
         # or run nix path-info <path>
 
         # is this generations /run/booted-system points
-        self.is_booted_sys: bool = self.nix_path == BOOTED_SYS_NIX_PATH
+        self.is_booted_sys: Final[bool] = self.nix_path == BOOTED_SYS_NIX_PATH
         # is this generations /run/current-system points
-        self.is_current_sys: bool = self.nix_path == CURRENT_SYS_NIX_PATH
+        self.is_current_sys: Final[bool] = (
+            self.nix_path == CURRENT_SYS_NIX_PATH
+        )
         # whether there is current tags in nix-env --profile
-        self.is_current_profile: bool = is_current_profile
+        self.is_current_profile: Final[bool] = is_current_profile
 
-        self.datetime_ = datetime_
+        self.datetime_: Final[datetime] = datetime_
 
-        self.entry_name = f"{BOOT_ENTRY_PREFIX}{self.number}.conf"
-        self.entry_path = Path(BOOT_ENTRY_PATH, self.entry_name)
+        self.entry_name: Final[str] = f"{BOOT_ENTRY_PREFIX}{self.number}.conf"
+        self.entry_path: Final[Path] = Path(BOOT_ENTRY_PATH, self.entry_name)
 
+    @override
     def __str__(self):
         return f"{self.number} {self.datetime_}"
 
-    def __lt__(self, other):
+    def __lt__(self, other: NixGeneration):
         return self.datetime_ < other.datetime_
 
+    @override
     def __hash__(self):
         return hash(frozenset({self.datetime_, self.number}))
 
+    @override
     def __eq__(self, other):
-        return self.__hash == other.__hash__
+        return self.__hash__ == other.__hash__
 
     def remove_boot_entry(self, *, run: bool = False):
         if not self.entry_path.is_file():
@@ -92,7 +110,7 @@ def get_generations() -> (
     ]
 ):
     """
-    Return (current_profile_gen, booted_sys_gen, current_sys_gen generation_map[key: date, list[generations]])
+    Return (current_profile_gen, booted_sys_gen, current_sys_gen generation_map[key: datetime, list[generations]])
     """
 
     args = (
@@ -100,7 +118,7 @@ def get_generations() -> (
         "--profile",
         "/nix/var/nix/profiles/system",
         "--list-generations",
-    )
+    )  # local timezone 으로 출력함.
 
     proc = subprocess.run(args, check=True, capture_output=True, text=True)
 
@@ -115,23 +133,26 @@ def get_generations() -> (
 
     for line in proc.stdout.splitlines():
         gen: NixGeneration
+        datetime_: datetime
 
         if line.endswith("(current)"):
             if current_profile_gen is not None:
                 msg = "Multiple current generation in output"
                 raise Exception(msg)
 
-            number, date_, time, _ = line.split()
+            number, datestr, timestr, _ = line.split()
+            datetime_ = datetime.fromisoformat(f"{datestr}T{timestr}")
             gen = NixGeneration(
                 int(number),
-                datetime.fromisoformat(f"{date_}T{time}"),
+                datetime_,
                 is_current_profile=True,
             )
         else:
-            number, date_, time = line.split()
+            number, datestr, timestr = line.split()
+            datetime_ = datetime.fromisoformat(f"{datestr}T{timestr}")
             gen = NixGeneration(
                 int(number),
-                datetime.fromisoformat(f"{date_}T{time}"),
+                datetime_,
                 is_current_profile=False,
             )
 
@@ -144,18 +165,17 @@ def get_generations() -> (
         if gen.is_current_sys:
             current_sys_gen = gen
 
-        generations_by_date[date.fromisoformat(f"{date_}")].add(gen)
+        gen_date = (datetime_ - DAY_ADJUST).date()
+        generations_by_date[gen_date].add(gen)
 
     if booted_sys_gen is None:
-        msg = "WARNING: Boot system's generation does not exists"
-        print(msg)
+        logger.warning("Boot system's generation does not exists")
 
     if current_sys_gen is None:
-        msg = "WARNING: Current system's generation does not exists"
-        print(msg)
+        logger.warning("Current system's generation does not exists")
 
     if current_profile_gen is None:
-        msg = "ERROR: No current generation in output"
+        msg = "No current generation in output"
         raise Exception(msg)
 
     return (
@@ -197,15 +217,15 @@ def remove_profile(
     for g in generations:
         args.extend(["--delete-generations", f"{g.number}"])
 
-    subprocess.run(args, check=True)
+    _ = subprocess.run(args, check=True)
 
     return True
 
 
 def get_args() -> Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("delThreshold", default=14, type=int)
-    parser.add_argument("--run", action="store_true")
+    _ = parser.add_argument("delThreshold", default=14, type=int)
+    _ = parser.add_argument("--run", action="store_true")
     return parser.parse_args()
 
 
@@ -216,9 +236,9 @@ def main() -> int:
     current_profile_gen, booted_sys_gen, current_sys_gen, map_ = (
         get_generations()
     )
-    current_profile_date = current_profile_gen.datetime_.date()
+    current_profile_date = (current_profile_gen.datetime_ - DAY_ADJUST).date()
 
-    today = datetime.now(timezone.utc).astimezone().date()
+    today = (datetime.now() - DAY_ADJUST).date()
 
     # remove generations without boot-entry
     # for generations in map_.values():
