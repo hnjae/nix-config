9bba770e82ddb5d9040328c94de65bd5f7c235a6
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-03-31 20:57:28 +0900

    isis: 2025-03-31T20:57:28+09:00

--- modules/profiles/base-nixos/role/desktop/xremap.nix
+++ modules/profiles/base-nixos/role/desktop/xremap.nix
@@ -37,24 +37,19 @@ in
         name = "Global";
         remap = {
           "CapsLock" = "BackSpace"; # xkboption 으로 설정하면, chromium+wayland 에서는 적용되지 않음
+        };
+      }
+      {
+        name = "ms-designer-compact-keyboard";
+        remap = {
           # NOT Hangul_Hanja
           # NOTE: 이거 이름 어떻게 찾냐.. xkb 랑 일치하지 않음... <2025-03-23>
           # <https://github.com/xremap/xremap/discussions/356>
           # "Hanja" = "Alt_L"; # MS Keyboard;
           "Hanja" = "space"; # MS Keyboard;
         };
+        device.only = [ "Designer Compact Keyboard" ];
       }
-      /*
-        {
-          name = "ms-designer-compact-keyboard";
-          remap = {
-          };
-          device = {
-            only = "Designer Compact Keyboard";
-          };
-        }
-      */
-
       # NOTE: gnome 이 디바이스별 설정을 지원하지 않아 xremap 단에서 처리. <2025-03-30>
       {
         name = "left-handed-mouse";
