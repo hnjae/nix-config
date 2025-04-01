2b3c0d9ac8863b519d047a74606f53c0445f5823
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-04-01 22:27:21 +0900

    isis: 2025-04-01T22:27:21+09:00

--- modules/profiles/base-nixos/configs/nix/managing.nix
+++ modules/profiles/base-nixos/configs/nix/managing.nix
@@ -27,9 +27,9 @@ in
         ."${cfg.role}";
   };
 
-  # services.nix-store-gc = {
-  #   enable = mkOverride 999 config.base-nixos.role == "desktop";
-  # };
+  services.nix-store-gc = {
+    enable = mkOverride 999 false;
+  };
 
   nix.gc = {
     # run nix-collect-garbage
