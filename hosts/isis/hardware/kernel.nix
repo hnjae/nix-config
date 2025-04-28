3b9cb9f1e5956953d3a3632ef47440ed5811fd62
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-04-28 22:07:51 +0900

    isis: 2025-04-28T22:07:49+09:00

--- hosts/isis/hardware/kernel.nix
+++ hosts/isis/hardware/kernel.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 {
   # https://www.kernel.org/
+  # https://www.kernel.org/category/releases.html
   # https://github.com/openzfs/zfs/releases/
   # boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.kernelPackages = pkgs.linuxPackages_6_13;
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
 }
