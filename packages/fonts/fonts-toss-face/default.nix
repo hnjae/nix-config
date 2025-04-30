2e81f1b5f28106abfe20a4430354e2d677f89b14
KIM Hyunjae <hnjae@outlook.com>
2025-04-30 22:50:27 +0900

    isis: 2025-04-30T22:50:18+09:00

--- packages/fonts/fonts-toss-face/default.nix
+++ packages/fonts/fonts-toss-face/default.nix
@@ -1,26 +1,4 @@
 {
-  stdenv,
-  lib,
+  pkgs ? import <nixpkgs> { },
 }:
-stdenv.mkDerivation {
-  pname = "fonts-toss-face";
-  version = "1.6"; # 2023-12-29 checked
-
-  src = builtins.fetchurl {
-    url = "https://github.com/toss/tossface/releases/latest/download/TossFaceFontMac.ttf";
-    sha256 = "sha256:10s8bcz2fqscflyffbkc5b31432y8dk0mkxw8h4brid9pqqwyjgc";
-  };
-  dontUnpack = true;
-
-  installPhase = ''
-    mkdir -p "$out/share/fonts/truetype"
-    cp "$src" "$out/share/fonts/truetype/TossFaceFontMac.ttf"
-  '';
-
-  meta = with lib; {
-    description = "함께 만들어가는, 토스페이스.";
-    homepage = "https://toss.im/tossface";
-    license = licenses.unfree; # https://toss.im/tossface/copyright
-    platforms = platforms.all;
-  };
-}
+pkgs.callPackage ./derivation.nix { }
---
+++ packages/fonts/fonts-toss-face/derivation.nix
@@ -0,0 +1,28 @@
+{
+  stdenv,
+  lib,
+}:
+stdenv.mkDerivation {
+  pname = "fonts-toss-face";
+  version = "1.6.1"; # 2025-04-30 checked
+
+  src = builtins.fetchurl {
+    # url = "https://github.com/toss/tossface/releases/latest/download/TossFaceFontMac.ttf";
+    url = "https://github.com/toss/tossface/releases/download/v1.6.1/TossFaceFontMac.ttf";
+    sha256 = "sha256:10s8bcz2fqscflyffbkc5b31432y8dk0mkxw8h4brid9pqqwyjgc";
+  };
+  dontUnpack = true;
+
+  installPhase = ''
+    mkdir -p "$out/share/fonts/truetype"
+    cp "$src" "$out/share/fonts/truetype/TossFaceFontMac.ttf"
+  '';
+
+  meta = with lib; {
+    description = "함께 만들어가는, 토스페이스.";
+    homepage = "https://toss.im/tossface";
+    # homepage = "https://github.com/toss/tossface";
+    license = licenses.unfree; # https://toss.im/tossface/copyright#full
+    platforms = platforms.all;
+  };
+}
