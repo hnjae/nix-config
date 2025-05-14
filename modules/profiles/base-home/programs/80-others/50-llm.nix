e9c92bd3d613dc0e59acafb691a4a5fc9f9a0525
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-05-14 12:27:58 +0900

    isis: 2025-05-14T12:27:57+09:00

--- modules/profiles/base-home/programs/80-others/50-llm.nix
+++ modules/profiles/base-home/programs/80-others/50-llm.nix
@@ -1,10 +1,22 @@
-{ pkgsUnstable, ... }:
 {
-  home.packages = builtins.concatLists [
-    (with pkgsUnstable; [
-      tgpt # support openai, ollma https://github.com/aandrew-me/tgpt
-      gtt # can use chatgpt to translate <https://github.com/eeeXun/gtt>
-      # oterml # ollama client - not in nixpkgs 2024-08-07
-    ])
-  ];
+  config,
+  lib,
+  pkgsUnstable,
+  ...
+}:
+let
+  baseHomeCfg = config.base-home;
+in
+{
+  config = lib.mkIf (baseHomeCfg.isDesktop) {
+    home.packages = builtins.concatLists [
+      (with pkgsUnstable; [
+        tgpt # support openai, ollma https://github.com/aandrew-me/tgpt
+        gtt # can use chatgpt to translate <https://github.com/eeeXun/gtt>
+        # oterml # ollama client - not in nixpkgs 2024-08-07
+
+        jan # GUI
+      ])
+    ];
+  };
 }
