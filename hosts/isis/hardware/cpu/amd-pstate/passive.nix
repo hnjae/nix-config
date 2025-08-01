31acbef000242660a27a619b9f8378c9ac9cdfdb
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-08-01 16:39:15 +0900

    isis: 2025-08-01T16:39:15+09:00

--- hosts/isis/hardware/cpu/amd-pstate/passive.nix
+++ hosts/isis/hardware/cpu/amd-pstate/passive.nix
@@ -42,8 +42,8 @@ in
     CPU_SCALING_GOVERNOR_ON_AC = cpuScalingGovernor.performance;
     CPU_SCALING_GOVERNOR_ON_BAT = cpuScalingGovernor.schedutil;
 
-    PLATFORM_PROFILE_ON_AC = "performance";
-    # PLATFORM_PROFILE_ON_BAT = "balanced";
+    # PLATFORM_PROFILE_ON_AC = "performance";
+    PLATFORM_PROFILE_ON_AC = "balanced";
     PLATFORM_PROFILE_ON_BAT = "low-power";
   };
 
