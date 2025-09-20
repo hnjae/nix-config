117e637f3b4cf0909f04d2d36cbd19f2e2420a5d
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-09-20 20:08:37 +0900

    feat(lact): remove lact module

--- modules/nixos-modules/flake-module.nix
+++ modules/nixos-modules/flake-module.nix
@@ -1,11 +1,7 @@
-{ inputs, ... }:
 {
   flake.nixosModules = {
     # services
     nixos-generation-gc = import ./services/nixos-generation-gc;
     nix-store-gc = import ./services/nix-store-gc;
-
-    # programs
-    lact = import ./programs/lact.nix;
   };
 }
