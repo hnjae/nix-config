4d703ecb9f084d6c8549af04f1c2f279a16cea9f
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-11-30 19:20:13 +0900

    osiris: 2025-11-30T19:20:13+09:00

--- hosts/osiris/hardware/power.nix
+++ hosts/osiris/hardware/power.nix
@@ -20,6 +20,13 @@
     SUBSYSTEM=="ata_port", KERNEL=="ata*", ATTR{device/power/control}="auto"
 
     ACTION=="add|change", KERNEL=="sd[a-z]", ATTRS{queue/rotational}=="1", RUN+="${pkgs.hdparm}/sbin/hdparm -B 128 -S 248 /dev/%k"
+
+    # Disable wake on USB for Mouse
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{product}=="*Mouse*", ATTR{power/wakeup}="disabled"
+
+    # Enable wake on USB for Keyboard
+    ACTION=="add", SUBSYSTEM=="usb", ATTRS{product}=="Bluetooth Radio", ATTR{power/wakeup}="enabled"
+    ACTION=="add", SUBSYSTEM=="usb", ATTRS{product}=="*Keyboard", ATTR{power/wakeup}="enabled"
   '';
 
   # NOTE: 248 = 4 hours
