8bb99a69cc472cdbe9058880ac8ec97e52b8fd53
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-12-05 17:30:58 +0900

    osiris: 2025-12-05T17:30:58+09:00

--- hosts/osiris/services/systemd-mounts.nix
+++ hosts/osiris/services/systemd-mounts.nix
@@ -12,27 +12,27 @@
   ];
 
   systemd.mounts = [
-    {
-      what = "eris:vault";
-      type = "nfs";
-      where = "/media/vault";
-      after = [
-        "tailscaled.service"
-        "network-online.target"
-        "systemd-resolved.service"
-      ];
-      wants = [
-        "tailscaled.service"
-        "network-online.target"
-        "systemd-resolved.service"
-      ];
-      mountConfig = {
-        Options = builtins.concatStringsSep "," [
-          "nfsvers=4"
-          "rsize=1048576"
-          "wsize=1048576"
-        ];
-      };
-    }
+    # {
+    #   what = "eris:vault";
+    #   type = "nfs";
+    #   where = "/media/vault";
+    #   after = [
+    #     "tailscaled.service"
+    #     "network-online.target"
+    #     "systemd-resolved.service"
+    #   ];
+    #   wants = [
+    #     "tailscaled.service"
+    #     "network-online.target"
+    #     "systemd-resolved.service"
+    #   ];
+    #   mountConfig = {
+    #     Options = builtins.concatStringsSep "," [
+    #       "nfsvers=4"
+    #       "rsize=1048576"
+    #       "wsize=1048576"
+    #     ];
+    #   };
+    # }
   ];
 }
