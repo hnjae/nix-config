3ed9e4e3f45767d421d1ceb0190088a8326d2f23
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-12-14 00:25:29 +0900

    chore(wincompat-rename): add devshell environment

--- apps/wincompat-rename/flake-module.nix
+++ apps/wincompat-rename/flake-module.nix
@@ -1,3 +1,6 @@
+let
+  project = "wincompat-rename";
+in
 {
   perSystem =
     { pkgs, config, ... }:
@@ -5,53 +8,57 @@
       inherit (pkgs) rustPlatform;
     in
     {
-      packages = {
-        wincompat-rename = import ./. { inherit pkgs; };
+      packages."${project}" = import ./. { inherit pkgs; };
+
+      checks."${project}" = rustPlatform.buildRustPackage {
+        pname = "${project}-checks";
+        version = "0.1.0";
+        src = ./.;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+
+        nativeBuildInputs = with pkgs; [
+          rustfmt
+          clippy
+        ];
+
+        preCheck = ''
+          echo 'INFO: Running cargo fmt check...' >&2
+          cargo fmt --check
+
+          echo 'INFO: Running cargo clippy (linter)...' >&2
+          cargo clippy --all-targets --all-features
+        '';
+
+        checkPhase = ''
+          runHook preCheck
+
+          echo "INFO: Running cargo test..." >&2
+          cargo test --release
+
+          runHook postCheck
+        '';
+
+        doCheck = true;
+        dontInstall = true;
+        installPhase = "touch $out";
       };
 
-      checks = {
-        wincompat-rename = rustPlatform.buildRustPackage {
-          pname = "wincompat-rename-checks";
-          version = "0.1.0";
-          src = ./.;
-
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-
-          nativeBuildInputs = with pkgs; [
-            rustfmt
-            clippy
-          ];
-
-          preCheck = ''
-            echo 'INFO: Running cargo fmt check...' >&2
-            cargo fmt --check
-
-            echo 'INFO: Running cargo clippy (linter)...' >&2
-            cargo clippy --all-targets --all-features
-          '';
-
-          checkPhase = ''
-            runHook preCheck
-
-            echo "INFO: Running cargo test..." >&2
-            cargo test --release
-
-            runHook postCheck
-          '';
-
-          doCheck = true;
-          dontInstall = true;
-          installPhase = "touch $out";
-        };
+      apps."${project}" = {
+        type = "app";
+        program = config.packages."${project}";
       };
 
-      apps = {
-        wincompat-rename = {
-          type = "app";
-          program = config.packages.wincompat-rename;
-        };
+      devShells.${project} = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          cargo
+          cargo-tarpaulin # code coverage tool
+          clippy # official linter
+          rust-analyzer # (officia ) rust compiler front-end for IDEs
+          rustfmt # official formatter
+        ];
       };
     };
 }
