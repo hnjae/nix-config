17697d8f60f2366f013ce14b6399066fb60e99b1
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-12-26 16:56:31 +0900

    fix(eris): correct ha-esp's dependencies
    Change `config.services.nix.gc.automatic` to `config.nix.gc.automatic`
    to use the correct configuration attribute path.

--- hosts/eris/hardware/ha-esp.nix
+++ hosts/eris/hardware/ha-esp.nix
@@ -56,7 +56,7 @@ in
         "multi-user.target" # after boot
         (lib.lists.optional config.my.services.nixos-gc.enable "nixos-gc.service")
         (lib.lists.optional config.my.services.nix-store-gc.enable "nix-store-gc.service")
-        (lib.lists.optional config.services.nix.gc.automatic "nix-gc.service")
+        (lib.lists.optional config.nix.gc.automatic "nix-gc.service")
         (lib.lists.optional config.programs.nh.clean.enable "nh-clean.service")
       ];
       Wants = [
