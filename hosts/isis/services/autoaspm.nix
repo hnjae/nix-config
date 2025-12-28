686e4f5f5357c17e1f068d3f2fe4f4ede53eef59
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-12-28 13:30:13 +0900

    feat(isis): configure autospm device modes for AMD USB4 controller
    Enable l1 power state for AMD Family 19h USB4/Thunderbolt PCIe tunnel
    device.

--- hosts/isis/services/autoaspm.nix
+++ hosts/isis/services/autoaspm.nix
@@ -3,7 +3,11 @@
     enable = true;
     mode = "l0s"; # l0sl1 은 이 장비에서 불가능.
     deviceModes = {
-      # "17cb:1103" = "l0s"; # Network controller: Qualcomm Technologies, Inc QCNFA765 Wireless Network Adapter (rev 01); l0sl1 불가능
+      "1022:14ef" = "l1"; # Advanced Micro Devices, Inc. [AMD] Family 19h USB4/Thunderbolt PCIe tunnel
+      # "1022:14eb" = "l0sl1"; # Advanced Micro Devices, Inc. [AMD] Phoenix Internal GPP Bridge to Bus [C:A]
+
+      # l0sl1 을 설정하면 강제 종료되는 장비들:
+      # "17cb:1103" = "l0s"; # Network controller: Qualcomm Technologies, Inc QCNFA765 Wireless Network Adapter (rev 01)
     };
   };
 }
