74f31f252d4245441f7e9f08ae7ffac75e8d28a5
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-01-03 20:06:48 +0900

    osiris: 2026-01-03T20:06:47+09:00

--- hosts/eris/hardware/disk-config.nix
+++ hosts/eris/hardware/disk-config.nix
@@ -225,6 +225,14 @@
   systemd.coredump.extraConfig = ''
     Compress=no
   '';
+  /*
+    Extra config options for systemd-journald. See journald.conf(5) for available options.
+
+    /etc/systemd/journald.conf
+  */
+  services.journald.extraConfig = ''
+    Compress=no
+  '';
 
   # stateless 를 설정하면, cups 가 켜질때 /var/lib/cups 를 지워버림.
   # 어차피 `/` 가 tmpfs 라서 의미 없고, 복잡도 증가.
